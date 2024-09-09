8e8a700e209f1e7efd07f2aed82d44e93272cbf0
Anna Krasnova <akrasnova424@gmail.com>
2024-09-10 00:20:42 +0300

    Update aggregate_last_paid_click.sql

--- aggregate_last_paid_click.sql
+++ aggregate_last_paid_click.sql
@@ -1,25 +1,69 @@
+with last_visits as (
+    select
+        max(date(visit_date)) as visit_date,
+        visitor_id
+    from sessions
+    group by 2
+),
+
+leads as (
+    select
+        lv.visit_date,
+        s.source as utm_source,
+        s.medium as utm_medium,
+        s.campaign as utm_campaign,
+        count(distinct lv.visitor_id) as visitors_count,
+        count(l.lead_id) as leads_count,
+        count(
+            case
+                when l.status_id = 142 then lv.visitor_id
+            end
+        ) as purchases_count,
+        sum(amount) as revenue
+    from last_visits as lv
+    inner join sessions as s
+        on lv.visitor_id = s.visitor_id
+    left join leads as l
+        on s.visitor_id = l.visitor_id
+    where s.medium <> 'organic'
+    group by 1, 2, 3, 4
+),
+
+ads as (
+    select
+        date(campaign_date) as campaign_date,
+        utm_source,
+        utm_medium,
+        utm_campaign,
+        sum(daily_spent) as total_cost
+    from vk_ads
+    group by 1, 2, 3, 4
+    union all
+    select
+        date(campaign_date) as campaign_date,
+        utm_source,
+        utm_medium,
+        utm_campaign,
+        sum(daily_spent) as total_cost
+    from ya_ads
+    group by 1, 2, 3, 4
+)
+
 select
-    date(s.visit_date),
-    s.source as utm_source,
-    s.medium as utm_medium,
-    s.campaign as utm_campaign,
-    count(s.visitor_id) as visitors_count,
-    sum(coalesce(vk.daily_spent, 0))
-    + sum(coalesce(ya.daily_spent, 0)) as total_cost,
-    count(lead_id) as leads_count,
-    count(status_id) as purchases_count,
-    sum(amount) as revenue
-from sessions as s
-left join leads as l
-    on s.visitor_id = l.visitor_id
-left join vk_ads as vk
+    l.visit_date,
+    l.utm_source,
+    l.utm_medium,
+    l.utm_campaign,
+    l.visitors_count,
+    a.total_cost,
+    l.leads_count,
+    l.purchases_count,
+    l.revenue
+from leads as l
+left join ads as a
     on
-        s.source = vk.utm_source and s.medium = vk.utm_medium
-        and s.campaign = vk.utm_campaign and s.content = vk.utm_content
-left join ya_ads as ya
-    on
-        s.source = ya.utm_source and s.medium = ya.utm_medium
-        and s.campaign = ya.utm_campaign and s.content = ya.utm_content
-where s.medium <> 'organic' and status_id = 142
-group by 1, 2, 3, 4
-order by 9 desc nulls last, 1, 5 desc, 2, 3, 4;
+        l.visit_date = a.campaign_date
+        and l.utm_source = a.utm_source
+        and l.utm_medium = a.utm_medium
+        and l.utm_campaign = a.utm_campaign
+order by 9 desc nulls last, 1 asc, 5 desc, 2, 3, 4;
