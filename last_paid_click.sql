50d3f2ad8e63bb4aba3adf832493d09655ae3a72
Anna Krasnova <akrasnova424@gmail.com>
2024-09-10 14:25:58 +0300

    Update last_paid_click.sql

--- last_paid_click.sql
+++ last_paid_click.sql
@@ -1,14 +1,15 @@
 with last_visits as (
     select
         s.visitor_id,
-        max(s.visit_date) as visit_date
-    from sessions as s where s.medium <> 'organic'
+        max(s.visit_date) as max_visit_date
+    from sessions as s
+    where s.medium <> 'organic'
     group by 1
 )
 
 select
     lv.visitor_id,
-    lv.visit_date,
+    lv.max_visit_date as visit_date,
     s.source as utm_source,
     s.medium as utm_medium,
     s.campaign as utm_campaign,
@@ -19,7 +20,7 @@ select
     l.status_id
 from last_visits as lv
 left join sessions as s
-    on lv.visitor_id = s.visitor_id
+    on lv.visitor_id = s.visitor_id and lv.max_visit_date = s.visit_date
 left join leads as l
     on lv.visitor_id = l.visitor_id
 where s.medium <> 'organic'
