df696a4984654e4a1f46950336f2c30bdf69677c
Anna Krasnova <akrasnova424@gmail.com>
2024-09-30 15:11:42 +0300

    lint spaces

--- dashboard.sql
+++ dashboard.sql
@@ -67,25 +67,25 @@ select
     coalesce(a.total_cost / l.visitors_count, 0) as cpu,
     round(
         coalesce(l.leads_count, 0)::numeric / l.visitors_count::numeric * 100, 2
-        ) as leads_cr_percents,
+    ) as leads_cr_percents,
     case
         when
             l.leads_count is null or l.leads_count = 0
-        or l.purchases_count is null or l.purchases_count = 0 then 0
-    	else round(l.purchases_count::numeric / l.leads_count::numeric * 100, 2)
+            or l.purchases_count is null or l.purchases_count = 0 then 0
+      	else round(l.purchases_count::numeric / l.leads_count::numeric * 100, 2)
     end as purchases_cr_percents,
     case
-    	when
-            a.total_cost is null or a.total_cost = 0 or leads_count = 0 then 0
+        when
+            a.total_cost is null or a.total_cost = 0 or l.leads_count = 0 then 0
     	else a.total_cost / l.leads_count
-    end as cpl,
+        end as cpl,
     case
-    	when a.total_cost is null or a.total_cost = 0 or purchases_count = 0 then 0
+        when a.total_cost is null or a.total_cost = 0 or l.purchases_count = 0 then 0
     	else a.total_cost / l.purchases_count
     end as cppu,
     case
-    	when a.total_cost is null or a.total_cost = 0 then 0
-    	else ((l.revenue - a.total_cost) / a.total_cost) * 100
+        when a.total_cost is null or a.total_cost = 0 then 0
+        else ((l.revenue - a.total_cost) / a.total_cost) * 100
     end as roi
 from leads as l
 left join ads as a
